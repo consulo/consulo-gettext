289f290c71372ae346e2743c4df2d52eb38c6e9b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-18 12:53:19 +0300

    reduce ide deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,14 @@
  * @since 01-Apr-22
  */
 open module com.jetbrains.gettext {
-  requires consulo.ide.api;
+  requires consulo.application.api;
+  requires consulo.code.editor.api;
+  requires consulo.color.scheme.api;
+  requires consulo.language.api;
+  requires consulo.language.editor.api;
   requires consulo.language.impl;
+  requires consulo.localize.api;
+  requires consulo.project.api;
+  requires consulo.ui.api;
+  requires consulo.virtual.file.system.api;
 }
